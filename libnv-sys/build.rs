dea83ea2ed5beb516dd149d97b6934522ebe1654
Alan Somers <asomers@gmail.com>
2022-12-07 14:02:04 -0700

    Multiple build fixes
    * Use currently supported environments in CI
    * No need to install curl in CI
    * Source the rustup environment for each CI script
    * Don't bother with mdconfig.  That might be a copypasta?
    * Use a "path" dependency in Cargo.toml to pickup local changes to
      libnv-sys.
    * Fix the examples build when the nvpair feature is not in use.
    * Fix the "links" entry for libnv-sys.
    * Fix the build of libnv-sys on FreeBSD 14.

--- libnv-sys/build.rs
+++ libnv-sys/build.rs
@@ -1,3 +1,32 @@
+use std::{
+    env,
+    fs,
+    path::Path,
+    os::unix
+};
+
 fn main() {
-    println!("cargo:rustc-link-lib=nvpair");
+    // FreeBSD 14 adds a libnv.so.1, which is the same as libnv.so.0 except that
+    // its symbols are renamed so as not to collide with libnvpair.so's.  Always
+    // link to libnv.so.0.  That way out binaries will be cross-compilable and
+    // run on FreeBSD 11.0 or greater, and we can use the same symbol names for
+    // all OS versions.
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let link = Path::join(Path::new(&out_dir), "libnv.so");
+    match fs::read_link(&link) {
+        Ok(l) if l == link => (),
+        Ok(_) => {
+            fs::remove_file(&link).unwrap();
+            unix::fs::symlink("/lib/libnv.so.0", &link).unwrap();
+        },
+        Err(_) => {
+            unix::fs::symlink("/lib/libnv.so.0", &link).unwrap();
+        }
+    }
+    println!("cargo:rerun-if-env-changed=OUT_DIR");
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rustc-link-search=native={out_dir}");
+
+    // Link to libnv
+    println!("cargo:rustc-link-lib=nv");
 }
